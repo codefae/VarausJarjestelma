e9cbf9a025e4dbed6c3d509e6fee8b61148dcbce
Henri Auvinen <fheau002@edu.xamk.fi>
2025-03-03 13:38:54 +0200

    tehty validaattorit

--- Backend/src/Modules/ReservationModule/Shared/Dtos/OpenTimesDtoValidator.cs
+++ Backend/src/Modules/ReservationModule/Shared/Dtos/OpenTimesDtoValidator.cs
@@ -1,13 +1,19 @@
 using FastEndpoints;
+using FluentValidation;
 using FluentValidation.Results;
 
 namespace src.Modules.ReservationModule.Shared.Dtos;
 
 public class OpenTimesDtoValidator : Validator<OpenTimesDto>
 {
-    // TODO implement this
-    public override ValidationResult Validate(FluentValidation.ValidationContext<OpenTimesDto> context)
+    public OpenTimesDtoValidator()
     {
-        throw new NotImplementedException();
+        RuleFor(x => x.StartTime)
+            .LessThan(x => x.EndTime)
+            .WithMessage("StartTime must be earlier than EndTime");
+
+        RuleFor(x => x.EndTime)
+            .GreaterThan(x => x.StartTime)
+            .WithMessage("EndTime must be later than StartTime");
     }
-}
+} 
