52e226e49cdd7963efc6bd238a4795baa8252b61
eetu <feeha006@edu.xamk.fi>
2025-03-12 13:09:16 +0200

    Refactoring

--- Backend/src/Modules/ReservationModule/Features/DeleteReservation/DeleteReservationRequestValidator.cs
+++ Backend/src/Modules/ReservationModule/Features/DeleteReservation/DeleteReservationRequestValidator.cs
@@ -1,12 +1,11 @@
 using FastEndpoints;
-using FluentValidation.Results;
 
 namespace src.Modules.ReservationModule.Features.DeleteReservation;
 
 public class DeleteReservationRequestValidator : Validator<DeleteReservationRequest>
 {
-    public override ValidationResult Validate(FluentValidation.ValidationContext<DeleteReservationRequest> context)
-    {
-        throw new NotImplementedException();
-    }
+  public DeleteReservationRequestValidator()
+  {
+    throw new NotImplementedException();
+  }
 }
--- Backend/src/Modules/ReservationModule/Features/PatchReservationStartAndEndTimes/PatchReservationStartAndEndTimesValidator.cs
+++ Backend/src/Modules/ReservationModule/Features/PatchReservationStartAndEndTimes/PatchReservationStartAndEndTimesValidator.cs
@@ -1,12 +1,12 @@
+using System.ComponentModel.DataAnnotations;
 using FastEndpoints;
-using FluentValidation.Results;
 
 namespace src.Modules.ReservationModule.Features.PatchReservationStartAndEndTimes;
 
 public class PatchReservationStartAndEndTimesValidator : Validator<PatchReservationStartAndEndTimesRequest>
 {
-    public override ValidationResult Validate(FluentValidation.ValidationContext<PatchReservationStartAndEndTimesRequest> context)
-    {
-        throw new NotImplementedException();
-    }
+   public PatchReservationStartAndEndTimesValidator()
+   {
+      throw new NotImplementedException();
+   }
 }
