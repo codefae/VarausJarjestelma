ca14ec7e8bcf3cb7a946a18343ba5656545954b5
Aapo Kontiainen <faako005@edu.xamk.fi>
2025-03-21 09:19:47 +0200

    DeleteReservation

--- Backend/src/Modules/ReservationModule/Features/Admin/PostDeviceToRoom/PostDeviceToRoomRequestValidator.cs
+++ Backend/src/Modules/ReservationModule/Features/Admin/PostDeviceToRoom/PostDeviceToRoomRequestValidator.cs
@@ -1,11 +1,30 @@
 using FastEndpoints;
+using FluentValidation;
 
-namespace src.Modules.ReservationModule.Features.Admin.PostDeviceToRoom;
+namespace src.Modules.ReservationModule.Features.PostDeviceToRoom;
 
 public class PostDeviceToRoomRequestValidator : Validator<PostDeviceToRoomRequest>
 {
     public PostDeviceToRoomRequestValidator()
     {
-        throw new NotImplementedException();
+        RuleFor(x => x.UserId)
+            .NotEmpty().WithMessage("UserId is required.")
+            .Must(BeAValidGuid).WithMessage("UserId must be a valid GUID.");
+
+        RuleFor(x => x.DeviceDto.RoomId)
+            .NotEmpty().WithMessage("RoomId is required.")
+            .Must(BeAValidGuid).WithMessage("RoomId must be a valid GUID.");
+
+        RuleFor(x => x.DeviceDto.DeviceId)
+            .NotEmpty().WithMessage("DeviceId is required.")
+            .Must(BeAValidGuid).WithMessage("DeviceId must be a valid GUID.");
+
+        RuleFor(x => x.DeviceDto.DeviceName)
+            .NotEmpty().WithMessage("DeviceName is required.");
+    }
+
+    private bool BeAValidGuid(string value)
+    {
+        return Guid.TryParse(value, out _);
     }
 }
